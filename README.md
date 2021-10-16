c3fc11e42a86956728b279dd154fd7ad0af48e88
akashbansa2 <bansalakash210@gmail.com>
2021-10-16 13:30:43 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Project-Tech_Interacto
+# OUR FIRST PROJECT 
+# NOOB CODERS
---
+++ signup.php
@@ -0,0 +1,29 @@
+<?php
+
+
+include("connection.php");
+
+
+$userid = $_POST['userid'];
+$useremail = $_POST['useremail'];
+$userpass = $_POST['userpass'];
+$usercnfpass = $_POST['usercnfpass'];
+
+
+
+$query = "INSERT INTO signup ( userid, useremail, userpass, usercnfpass) VALUES ('$userid','$useremail','$userpass','$usercnfpass')";
+
+if($userpass==$usercnfpass)
+{
+mysqli_query($con, $query);
+}
+else{
+    echo "Incorrect Password";
+}
+
+if(isset($_POST['submitted']))
+{
+    header("Location: interact.html");
+}
+
+?>
